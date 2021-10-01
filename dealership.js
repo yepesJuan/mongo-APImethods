ca6c8cb0d22f5df3b6f85928a077f498fa534036
Juan Yepes <yepes.j@hotmail.com>
2021-10-01 15:04:32 -0400

    initial commit

---
+++ dealership.js
@@ -0,0 +1,93 @@
+import { MongoClient, ObjectId } from "mongodb";
+import dotenv from "dotenv";
+import faker from "faker";
+
+dotenv.config();
+let _client;
+
+// only ever create 1 client, if it already exists just return it;
+const createClient = async () => {
+  if (!_client) {
+    _client = new MongoClient(process.env.MONGO_URL);
+    await _client.connect();
+  }
+  return _client;
+};
+
+const getCarsCollection = async () => {
+  const dealer = await createClient();
+  const db = dealer.db("dealership");
+  return db.collection("cars");
+};
+
+const getBuyersCollection = async () => {
+  const dealer = await createClient();
+  const db = dealer.db("dealership");
+  return db.collection("cars");
+};
+
+const getOrdersCollection = async () => {
+  const dealer = await createClient();
+  const db = dealer.db("dealership");
+  return db.collection("orders");
+};
+
+const createCars = async ({ make, model, price }) => {
+  const carsCollection = await getCarsCollection();
+  return await carsCollection.insertOne({ make, model, price });
+  // await carsCollection.insertOne({ make, model, price });
+  // return { make, model, price };
+};
+
+const createBuyers = async ({ name, address, phone }) => {
+  const buyersCollection = await getBuyersCollection();
+  return await buyersCollection.insertOne({ name, address, phone });
+  // await buyersCollection.insertOne({ name, address, phone });
+  // return { name, address, phone };
+};
+
+const createOrders = async ({ date, carID, buyerID }) => {
+  const ordersCollection = await getOrdersCollection();
+  await ordersCollection.insertOne({ date, carID, buyerID });
+  return { date, carID, buyerID };
+};
+
+// const readOrder = async () => {
+//     const userCollection = await getCarsCollection();
+//     //const ret = await userCollection.find({"make": "CTS"});
+//     const ret = await userCollection.findOne({"_id": ObjectId("6156027771005f7645ea9a5e")});
+//    return ret // object single doc
+//     //return ret.toArray()
+//   };
+
+const run = async () => {
+  const client = await createClient();
+  //const orders = await readOrder();
+  //console.log(orders);
+
+  const newCar = await createCars({
+    make: faker.vehicle.model(),
+    model: faker.vehicle.type(),
+    price: faker.datatype.number(),
+  });
+  const newBuyer = await createBuyers({
+    name: `${faker.name.firstName()} ${faker.name.lastName}`,
+    address: faker.address.streetAddress(),
+    phone: faker.phone.phoneNumber(),
+  });
+  await createOrders({
+    date: faker.date.month(),
+    carID: newCar._id,
+    buyerID: newBuyer._id,
+  });
+  console.log(newCar);
+  console.log(newBuyer);
+  await client.close();
+};
+
+run().then();
+
+// to get ids you have to =
+//1.return on create functions line 37,43,49
+//2. create a variable for each on line 55,60
+// and console.log before closing connection
---
+++ index.js
@@ -0,0 +1,71 @@
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
+let _client;
+
+// connect to cluster to cache client connection;
+//function to keep using for each clientt
+// only create1 client, if already exits just return
+const createClient = async () => {
+  if (!_client) {
+    _client = new MongoClient(process.env.MONGO_URL);
+    await _client.connect();
+  }
+  return _client;
+};
+
+const getUserCollection = async () => {
+  const client = await createClient();
+  const db = client.db("db2");
+  return db.collection("user");
+};
+
+//
+const createUser = async ({ name, dob, email }) => {
+  const userCollection = await getUserCollection();
+  await userCollection.insertOne({ name, dob, email });
+  return { name, dob, email };
+};
+
+const run = async () => {
+  const client = await createClient();
+  await createUser({
+    name: "Juan",
+    dob: new Date("02/26/1997"),
+    email: "jy@hotmail.com",
+  })
+  await client.close();
+};
+
+run().then();
+
+/*
+
+const createUser = async ({name, dob, email}) => {
+  const client = await createClient()
+
+}
+const createUser = async ({name, dob, email}) => {
+  const client = await createClient()
+
+}
+const createUser = async ({name, dob, email}) => {
+  const client = await createClient()
+
+}
+
+
+
+createClient().then();
+
+// try {
+//   client = await createClient()
+
+//   if(!client){
+//       console.error("Something went horribly wrong")
+//     }
+//   }
+//  catch (error) {
+//   console.log("Oh No no no no no.", error)
+// } */
